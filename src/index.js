94700f17034dc1b6566ca3753875d80408a46eef
Andy Czerwonka <andy.czerwonka@gmail.com>
2016-07-16 09:59:15 -0600

    afetr section 11 "Reder Targets"

--- src/index.js
+++ src/index.js
@@ -1,15 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+// Create a new component that produces some HTML
+const App = () => {
+  return <div>Hi!</div>;
+}
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+// Take the component HTML and put it in the DOM
+ReactDOM.render(<App />, document.querySelector('.container'));
