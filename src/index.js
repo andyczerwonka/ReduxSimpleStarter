d3adf295efca74506fcadd518a66f1e7c3c0ee5c
Andy Czerwonka <andy.czerwonka@gmail.com>
2016-07-17 08:37:58 -0600

    lecture 15

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
 const API_KEY = 'AIzaSyClytfdi9jGJg4AUGWKHDr0jf_eUc-MMns';
 
 // Create a new component that produces some HTML
 const App = () => {
-  return <div>Hi!</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 }
 
 // Take the component HTML and put it in the DOM
