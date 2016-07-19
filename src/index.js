071a8a86a38b0994fd7c76cef40408711ff6bd07
Andy Czerwonka <andy.czerwonka@gmail.com>
2016-07-19 09:44:47 -0600

    lecture 22 complete

--- src/index.js
+++ src/index.js
@@ -1,17 +1,36 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyClytfdi9jGJg4AUGWKHDr0jf_eUc-MMns';
 
 // Create a new component that produces some HTML
-const App = () => {
-  return (
-    <div>
-      <SearchBar/>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      videos: []
+    };
+
+    YTSearch({
+      key: API_KEY,
+      term: 'surfboards'
+    }, videos => {
+      this.setState({videos});
+    });
+
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar/>
+      </div>
+    );
+  }
+
 }
 
 // Take the component HTML and put it in the DOM
