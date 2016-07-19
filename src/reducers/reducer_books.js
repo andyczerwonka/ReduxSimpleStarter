7c76d22750dcc6758e1fd43dc52bee6391b0cfcd
Andy Czerwonka <andy.czerwonka@gmail.com>
2016-07-19 14:37:20 -0600

    end of lecture 41, but can't we have a bug

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    {title: 'Javascript: The Good parts'},
+    {title: 'Javascript: Harry Potter'},
+    {title: 'Javascript: The Dark Tower'},
+    {title: 'Javascript: Eloquent Ruby'}
+  ]
+}
